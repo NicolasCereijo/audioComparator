3ea539cfb1a7acfbebec9cc323659f0b01d86b58
Nico <nicolascereijoranchal@gmail.com>
2023-09-20 14:15:35 +0200

    Variable names changed

--- main.py
+++ main.py
@@ -2,41 +2,43 @@ import librosa
 import pandas
 import numpy
 import matplotlib.pyplot as plt
-import seaborn as sns
+import seaborn
 from itertools import cycle
 
-sns.set_theme(style="white", palette=None)
+seaborn.set_theme(style = "white", palette = None)
 color_pal = plt.rcParams["axes.prop_cycle"].by_key()["color"]
 color_cycle = cycle(plt.rcParams["axes.prop_cycle"].by_key()["color"])
 
-rawData, integerData = librosa.load(r"C:\Users\nicol\Music\Investigación\Papel celulosa\5x5cm\Papel_celulosa_5x5cm_usos_0_distancia_10cm_192000Hz.wav")
+audioSignal, sampleRate = librosa.load(r"C:\Users\nicol\Music\Investigación\Papel celulosa\5x5cm\Papel_celulosa_5x5cm_usos_0_distancia_10cm_192000Hz.wav")
 
-print(rawData)
-print(rawData.shape)
-print(integerData)
+print(audioSignal)
+print(audioSignal.shape)
+print(sampleRate)
 
-pandas.Series(rawData).plot(figsize = (10, 5), lw = 1, title = "Representación de ejemplo", color=color_pal[0])
+# Imagen del sonido
+pandas.Series(audioSignal).plot(figsize = (10, 5), lw = 1, title ="Representación de ejemplo", color = color_pal[0])
 plt.show()
 
-pandas.Series(rawData[30000:30500]).plot(figsize = (10, 5), lw = 1, title = "Representación de ejemplo aumentada", color = color_pal[1])
+# Imagen del sonido ampliada
+pandas.Series(audioSignal[30000:30500]).plot(figsize = (10, 5), lw = 1, title ="Representación de ejemplo aumentada", color = color_pal[1])
 plt.show()
 
 # Representación del espectrograma
-STFT = librosa.stft(rawData)
+STFT = librosa.stft(audioSignal)
 decibels = librosa.amplitude_to_db(numpy.abs(STFT), ref = numpy.max)
 
-figure, axes = plt.subplots(figsize=(10, 5))
-img = librosa.display.specshow(decibels, x_axis = "time", y_axis = "log", ax = axes)
-axes.set_title("Ejemplo de espectrograma", fontsize = 20)
-figure.colorbar(img, ax = axes, format = f"%0.2f")
+figure1, axes1 = plt.subplots(figsize = (10, 5))
+image1 = librosa.display.specshow(decibels, x_axis ="time", y_axis ="log", ax = axes1)
+axes1.set_title("Ejemplo de espectrograma", fontsize = 20)
+figure1.colorbar(image1, ax = axes1, format =f"%0.2f")
 plt.show()
 
 # Representación del espectrograma Mel
-S = librosa.feature.melspectrogram(y = rawData, sr = integerData, n_mels =128 * 2, )
-decibelsMel = librosa.amplitude_to_db(S, ref = numpy.max)
+melSpectrogram = librosa.feature.melspectrogram(y = audioSignal, sr = sampleRate, n_mels =128 * 2, )
+decibelsMel = librosa.amplitude_to_db(melSpectrogram, ref = numpy.max)
 
-figure, axes = plt.subplots(figsize = (10, 5))
-image = librosa.display.specshow(decibelsMel, x_axis = "time", y_axis = "log", ax = axes)
-axes.set_title('Ejemplo de espectrograma Mel', fontsize = 20)
-figure.colorbar(image, ax = axes, format = f'%0.2f')
+figure2, axes2 = plt.subplots(figsize = (10, 5))
+image2 = librosa.display.specshow(decibelsMel, x_axis ="time", y_axis ="log", ax = axes2)
+axes2.set_title('Ejemplo de espectrograma Mel', fontsize = 20)
+figure2.colorbar(image2, ax = axes2, format =f'%0.2f')
 plt.show()
