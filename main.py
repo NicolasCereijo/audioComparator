c3c8d39e59d84c93a484a4f86973761f47a28d00
Nico <nicolascereijoranchal@gmail.com>
2023-09-20 14:17:29 +0200

    Some spaces changed

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ print(audioSignal.shape)
 print(sampleRate)
 
 # Imagen del sonido
-pandas.Series(audioSignal).plot(figsize = (10, 5), lw = 1, title ="Representación de ejemplo", color = color_pal[0])
+pandas.Series(audioSignal).plot(figsize = (10, 5), lw = 1, title = "Representación de ejemplo", color = color_pal[0])
 plt.show()
 
 # Imagen del sonido ampliada
@@ -28,17 +28,17 @@ STFT = librosa.stft(audioSignal)
 decibels = librosa.amplitude_to_db(numpy.abs(STFT), ref = numpy.max)
 
 figure1, axes1 = plt.subplots(figsize = (10, 5))
-image1 = librosa.display.specshow(decibels, x_axis ="time", y_axis ="log", ax = axes1)
+image1 = librosa.display.specshow(decibels, x_axis = "time", y_axis = "log", ax = axes1)
 axes1.set_title("Ejemplo de espectrograma", fontsize = 20)
-figure1.colorbar(image1, ax = axes1, format =f"%0.2f")
+figure1.colorbar(image1, ax = axes1, format = f"%0.2f")
 plt.show()
 
 # Representación del espectrograma Mel
-melSpectrogram = librosa.feature.melspectrogram(y = audioSignal, sr = sampleRate, n_mels =128 * 2, )
+melSpectrogram = librosa.feature.melspectrogram(y = audioSignal, sr = sampleRate, n_mels = 128 * 2, )
 decibelsMel = librosa.amplitude_to_db(melSpectrogram, ref = numpy.max)
 
 figure2, axes2 = plt.subplots(figsize = (10, 5))
-image2 = librosa.display.specshow(decibelsMel, x_axis ="time", y_axis ="log", ax = axes2)
+image2 = librosa.display.specshow(decibelsMel, x_axis = "time", y_axis = "log", ax = axes2)
 axes2.set_title('Ejemplo de espectrograma Mel', fontsize = 20)
-figure2.colorbar(image2, ax = axes2, format =f'%0.2f')
+figure2.colorbar(image2, ax = axes2, format = f'%0.2f')
 plt.show()
