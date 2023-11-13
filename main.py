3e319bd1520efee381710b8280341612966dcef3
Nico <nicolascereijoranchal@gmail.com>
2023-11-13 17:49:51 +0100

    Refactored code

--- main.py
+++ main.py
@@ -1,40 +1,57 @@
-import librosa
-import pandas
-import matplotlib.pyplot as plt
-import seaborn
-from itertools import cycle
 import glob
-# Repository: https://github.com/gudgud96/frechet-audio-distance
+from itertools import cycle
+
+import librosa
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 from frechet_audio_distance import FrechetAudioDistance
+
+# Frechet repository: https://github.com/gudgud96/frechet-audio-distance
 # Package torchvision needed
 
 # PANN model
 frechet = FrechetAudioDistance(
     model_name="pann",
-    # Sample rate must be 8000, 16000 or 32000
     sample_rate=16000,
     use_pca=False,
     use_activation=False,
     verbose=False
 )
 
-referenceFilesPath = "referenceFiles/"
-fileToComparePath = "fileToCompare/"
-referenceFiles = glob.glob(referenceFilesPath + "*.wav")
-fileToCompare = glob.glob(fileToComparePath + "*.wav")
-audioSignal1, sampleRate1 = librosa.load(referenceFiles[0])
-audioSignal2, sampleRate2 = librosa.load(fileToCompare[0])
+reference_path = "referenceFiles/"
+compare_path = "fileToCompare/"
+reference_files = glob.glob(reference_path + "*.wav")
+compare_files = glob.glob(compare_path + "*.wav")
 
-print("Frechet distance: ", frechet.score(referenceFilesPath, fileToComparePath, dtype="float32"))
-print("Sample rate model: ", frechet.sample_rate)
-print("Sample rate audio 1: ", sampleRate1)
-print("Sample rate audio 2: ", sampleRate2)
 
-seaborn.set_theme(style="white", palette=None)
-color_pal = plt.rcParams["axes.prop_cycle"].by_key()["color"]
-color_cycle = cycle(plt.rcParams["axes.prop_cycle"].by_key()["color"])
+def load_audio(file_path):
+    audio_signal, sample_rate = librosa.load(file_path)
+    return audio_signal, sample_rate
 
-# Sound image
-pandas.Series(audioSignal1).plot(figsize=(10, 5), lw=1, title="", color=color_pal[0])
-pandas.Series(audioSignal2).plot(figsize=(10, 5), lw=1, title="Audios to compare", color=color_pal[1])
-plt.show()
+
+def plot_audio_series(audio_signal, title, color):
+    pd.Series(audio_signal).plot(figsize=(10, 5), lw=1, title=title, color=color)
+
+
+def main():
+    audio_signal1, sample_rate1 = load_audio(reference_files[0])
+    audio_signal2, sample_rate2 = load_audio(compare_files[0])
+
+    print("Frechet distance: ", frechet.score(reference_path, compare_path, dtype="float32"))
+    print("Sample rate model: ", frechet.sample_rate)
+    print("Sample rate audio 1: ", sample_rate1)
+    print("Sample rate audio 2: ", sample_rate2)
+
+    sns.set_theme(style="white", palette=None)
+    color_pal = plt.rcParams["axes.prop_cycle"].by_key()["color"]
+    color_cycle = cycle(plt.rcParams["axes.prop_cycle"].by_key()["color"])
+
+    # Sound image
+    plot_audio_series(audio_signal1, "", color_pal[0])
+    plot_audio_series(audio_signal2, "Audios to compare", color_pal[1])
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
